4596e2fa71cb900098a94e1349adec285186e3da
Arun Ganesan <aganesan94@gmail.com>
2016-10-22 23:40:35 -0400

    Added final version of docker-compose version 2

---
+++ resources/docker-compose-mysql/docker-mysql-connector/src/main/java/org/pictolearn/docker/mysql/MySQLConnection.java
@@ -0,0 +1,30 @@
+package org.pictolearn.docker.mysql;
+
+import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Map;
+
+/**
+ * Hello world!
+ *
+ */
+public class MySQLConnection {
+	public static void main(String[] args) throws Exception {
+		String ipAddr = InetAddress.getLocalHost().getHostName();
+
+		Map<String, String> env = System.getenv();
+		for (String envName : env.keySet()) {
+		    System.out.format("%s=%s%n", envName, env.get(envName));
+		}
+		String url = "jdbc:mysql://mysql:3306/sys?autoReconnect=false&useSSL=false";
+		String user = "root";
+		String password = "root";
+		System.out.println("Connecting to URL " + url );
+		// Load the Connector/J driver
+		Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+		// Establish connection to MySQL
+		Connection conn = DriverManager.getConnection(url, user, password);
+		System.out.println("Connection was successful");
+	}
+}
