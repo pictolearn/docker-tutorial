20ad1c8031ebfbbe3721e1c08b5e5a9960896d8b
Arun Ganesan <aganesan94@gmail.com>
2016-10-28 12:43:42 -0400

    Reordered files

---
+++ Usecase-4/docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -0,0 +1,12 @@
+package org.pictolearn.docker;
+
+public class HelloWorldPing {
+	
+	public static void main(String args[]) throws Exception{
+		for(int i = 0 ; i < 100; i++){
+			System.out.println("Hello World Ping " + i );
+			Thread.sleep(1000);
+		}
+	}
+
+}
