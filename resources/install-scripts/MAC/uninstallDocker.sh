20c1a76330ae19e6bfaabadd9977c421b9b83152
Arun Ganesan <aganesan94@gmail.com>
2016-11-29 09:45:21 -0800

    removed unnecessary scripts

--- resources/install-scripts/MAC/uninstallDocker.sh
+++ resources/install-scripts/MAC/uninstallDocker.sh
@@ -3,3 +3,9 @@ docker-machine rm -f $(docker-machine ls -q);
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images)
+sudo rm -rf /Applications/Docker
+sudo rm -f /usr/local/bin/docker
+sudo rm -f /usr/local/bin/boot2docker
+sudo rm -f /usr/local/bin/docker-machine
+sudo rm -r /usr/local/bin/docker-machine-driver*
+sudo rm -f /usr/local/bin/docker-compose
