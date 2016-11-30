e3e6e51d630338ce197f33889e0e3bf8e333137c
Arun Ganesan <aganesan94@gmail.com>
2016-11-30 09:58:04 -0800

    install docker

--- resources/install-scripts/ubuntu/installDocker.sh
+++ resources/install-scripts/ubuntu/installDocker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Generic instructions to upgrade UBUNTU and add the repository URL for docker.
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get -y update
+sudo apt-get -y install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >> /etc/apt/sources.list.d/docker.list
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-cache policy docker-engine
-sudo apt-get update
-sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get -y update
+sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
 
 # Instructions to install docker
 sudo apt-get -y install docker
@@ -17,4 +17,4 @@ sudo apt-get -y install docker-compose
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo service docker restart
-sudo reboot
+#sudo reboot
