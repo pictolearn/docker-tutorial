4fae9cb1e031ce2095a718b45bccb72f2ae6d592
Arun Ganesan <aganesan94@gmail.com>
2017-03-21 22:14:47 -0400

    Updated use case 3 read me

--- Usecase-3/docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ Usecase-3/docker-git-hello-world/src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -4,7 +4,7 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 100; i++){
-			System.out.println("New Hello World Ping " + i );
+			System.out.println("Hello World Ping " + i );
 			Thread.sleep(1000);
 		}
 	}
