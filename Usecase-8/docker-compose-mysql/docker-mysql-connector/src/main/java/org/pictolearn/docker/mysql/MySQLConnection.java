4020ccaf296d41e471b50858d9094931a3fcac78
Arun Ganesan <aganesan94@gmail.com>
2017-03-29 21:55:05 -0400

    Updated scripts

--- Usecase-8/docker-compose-mysql/docker-mysql-connector/src/main/java/org/pictolearn/docker/mysql/MySQLConnection.java
+++ Usecase-8/docker-compose-mysql/docker-mysql-connector/src/main/java/org/pictolearn/docker/mysql/MySQLConnection.java
@@ -6,26 +6,39 @@ import java.sql.DriverManager;
 import java.util.Map;
 
 /**
- * Hello world!
+ * MYSQL connection checker, tries to connect to mysql database.
  *
  */
 public class MySQLConnection {
 	public static void main(String[] args) throws Exception {
 		String ipAddr = InetAddress.getLocalHost().getHostName();
-
+		System.out.println("Printing IP address of the host " + ipAddr);
 		Map<String, String> env = System.getenv();
 		for (String envName : env.keySet()) {
-		    System.out.format("%s=%s%n", envName, env.get(envName));
+			System.out.format("%s=%s%n", envName, env.get(envName));
 		}
 		Thread.sleep(10000);
-		String url = "jdbc:mysql://mysql:3306/Users?autoReconnect=false&useSSL=false";
-		String user = "root";
-		String password = "root";
-		System.out.println("Connecting to URL " + url );
-		// Load the Connector/J driver
-		Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-		// Establish connection to MySQL
-		Connection conn = DriverManager.getConnection(url, user, password);
-		System.out.println("Connection was successful");
+
+		boolean connected = false;
+		while (!connected) {
+			try {
+				
+				// Note the way the mysql container is used here.
+				String url = "jdbc:mysql://mysql:3306/Users?autoReconnect=false&useSSL=false";
+				String user = "root";
+				String password = "root";
+				System.out.println("Connecting to URL " + url);
+				// Load the Connector/J driver
+				Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+				// Establish connection to MySQL
+				Connection conn = DriverManager.getConnection(url, user, password);
+				System.out.println("Connection was successful");
+				connected = true;
+			} catch (Exception e) {
+				System.err.println("Error connecting to database");
+				Thread.sleep(1000);
+			}
+		}
+
 	}
 }
